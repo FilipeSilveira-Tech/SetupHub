c9a3aea44b1d88fd72643497ffdcd2fe4f28e564
Filipe Silveira <contato@filipesilveira.dev>
2026-07-11 12:57:18 -0300

    first commit

---
+++ scripts/paths.js
@@ -0,0 +1,21 @@
+export const PATHS = {
+  icons: {
+    navegadores: "./src/assets/icon/navegadores",
+    comunicacao: "./src/assets/icon/comunicacao",
+    desenvolvimento: "./src/assets/icon/desenvolvimento",
+    ides: "./src/assets/icon/ides",
+    produtividade: "./src/assets/icon/produtividade",
+    multimidia: "./src/assets/icon/multimidia",
+    jogos: "./src/assets/icon/jogos",
+    compactadores: "./src/assets/icon/compactadores",
+    utilitarios: "./src/assets/icon/utilitarios",
+    drivers: "./src/assets/icon/drivers",
+    seguranca: "./src/assets/icon/seguranca",
+    fontes: "./src/assets/icon/fontes",
+  },
+  logos: {
+    logoHorizontal: "./src/assets/logo-horizontal.png",
+    logoHorizontalGrande: "./src/assets/logo-horizontal-grande.png",
+    logoIcon: "./src/assets/logo-icon.png",
+  },
+};
